aa3663af6f52f8f501a28f25a82e8de5004e9a8e
JoseTorresDevCode <SgtJoseTorres@yahoo.com>
2020-09-17 11:18:06 -0500

    init

---
+++ Trash Colllector/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Trash_Colllector.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
