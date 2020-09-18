145410dd3ad9128c1747519bd8abff17be6cb3c6
JoseTorresDevCode <SgtJoseTorres@yahoo.com>
2020-09-18 09:05:38 -0500

    added some properties to Employee model

--- Trash Colllector/Models/Customer.cs
+++ Trash Colllector/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,13 @@ namespace Trash_Colllector.Models
 {
     public class Customer
     {
+        [Key]
         public int Id { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
         public string address { get; set; }
         public string state { get; set; }
-        public string zipCode { get; set; }
+        public int zipCode { get; set; }
         public string pickUpDay { get; set; }
 
     }
--- Trash Colllector/Models/Employee.cs
+++ Trash Colllector/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,9 @@ namespace Trash_Colllector.Models
 {
     public class Employee
     {
+        [Key]
+        public int employeeId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
     }
 }
