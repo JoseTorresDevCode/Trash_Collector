d595803953690e1ab1a315b5480af6106b6e9a4d
JoseTorresDevCode <SgtJoseTorres@yahoo.com>
2020-09-18 08:00:58 -0500

    added some model properties to customer

---
+++ Trash Colllector/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trash_Colllector.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string state { get; set; }
+        public string zipCode { get; set; }
+        public string pickUpDay { get; set; }
+
+    }
+}
